e97cd34d8b6b83989a670bb0dba78c58cd593834
LorennFdz <loreenfernandez.14@gmail.com>
2024-08-20 18:46:33 -0300

    arreglando problemas de rendimiento

--- src/components/Gral/Banners/Banners.jsx
+++ src/components/Gral/Banners/Banners.jsx
@@ -1,42 +1,42 @@
 export function BannerCart() {
   return (
     <figure>
-      <img src="/img/cart/banner-cart.png" alt="" />
+      <img src="/img/cart/banner-cart.png" alt="Banner Carrito" />
     </figure>
   )
 }
 export function BannerDrinks() {
   return (
     <figure>
-      <img src="/img/products/banner-bebidas.webp" alt="" />
+      <img src="/img/products/banner-bebidas.webp" alt="Banner Bebidas" />
     </figure>
   )
 }
 export function BannerFoods() {
   return (
     <figure>
-      <img src="img/products/banner-almacen.webp" alt="" />
+      <img src="img/products/banner-almacen.webp" alt="Banner Almacén" />
     </figure>
   )
 }
 export function BannerFrozen() {
   return (
     <figure>
-      <img src="/img/products/banner-frozen.webp" alt="" />
+      <img src="/img/products/banner-frozen.webp" alt="Banner Congelados" />
     </figure>
   )
 }
 export function BannerHome() {
   return (
     <figure>
-      <img src="/img/gral/banner-home.png" alt="" />
+      <img src="/img/gral/banner-home.png" alt="Banner Inicio" />
     </figure>
   )
 }
 export function BannerProducts() {
   return (
     <figure>
-      <img src="/img/gral/banner-products.png" alt="" />
+      <img src="/img/gral/banner-products.png" alt="Banner Productos" />
     </figure>
   )
 }
